a5f9b900b09fd84a0d28cb724a8632de0780a1a1
Chris Phillips <C_L_Phillips72@hotmail.com>
2021-11-28 22:25:18 -0600

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,14 +1,22 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { animals } from './animals';
 import { shoppingPartners } from './shoppingPartners';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             animals,
             shoppingPartners,
             partners,
@@ -17,5 +25,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store);
+
+    return { persistor, store };
 }
