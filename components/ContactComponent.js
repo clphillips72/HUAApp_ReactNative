861f660da0bd688fad40b2394f59028ae11ae9d2
Chris Phillips <C_L_Phillips72@hotmail.com>
2021-12-04 17:41:04 -0600

    Local Notifications

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -11,39 +11,41 @@ class Contact extends Component {
     render() {
         return(
             <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
-                <View>
-                    <Text style={styles.loadingHeader}>Contact HUA</Text>
-                    <Text style={styles.loadingText}>By Mail</Text>
+                <ScrollView>
                     <View>
-                        <Text style={styles.loadingText2}>You can write us at:</Text>
-                        <Text>Hearts United for Animals</Text>
-                        <Text>Box 286</Text>
-                        <Text>Auburn, NE 68305
-                        </Text>
+                        <Text style={styles.loadingHeader}>Contact HUA</Text>
+                        <Text style={styles.loadingText}>By Mail</Text>
+                        <View>
+                            <Text style={styles.loadingText2}>You can write us at:</Text>
+                            <Text>Hearts United for Animals</Text>
+                            <Text>Box 286</Text>
+                            <Text>Auburn, NE 68305
+                            </Text>
+                        </View>
+                        <Text style={styles.loadingText}>By Email</Text><Text></Text>
+                        <Text>You can email us at hua@hua.org.  Email inquiries receive the quickest response. </Text><Text></Text> 
+
+                        <Text>Please don't send email with attachments unless it's very clear in the subject line what the attachment is (like an application). We get thousands of emails a day and delete the ones that may be a virus. For the same reason, be sure to include a subject line that doesn't say "Hi" (or something similar) that may appear to be a virus.</Text>
+                        <Text style={styles.loadingText}>By Phone or Fax</Text>
+                        <Text>You can call us at 402.274.3679.</Text>
+
+                        <Text>You can send a fax to 402.274.3689.</Text>
+
+                        <Text>We'll be waiting!</Text>
+                        <Text>Visit HUA</Text>
+                        <Text>Visitation Policy</Text>
+                        <Text>Once your application is fully approved, we will reach out to schedule a time to visit the shelter and meet your new best friend. Adoption appointment hours are 10 am to 2 pm every day except Wednesdays. We are volunteer administration and need to make sure someone will be available to help you when you arrive. For volunteering and tours, please write to hua@hua.org to schedule a time to visit and obtain directions. We are in the country between Auburn and Nebraska City. We cannot be located via any mapping programs.</Text>
+                        <Text>INCLUDE HUA IN ESTATE PLANNING</Text>
+                        <Text>If you would like to include Hearts United for Animals in your will or trust, please use our legal name and federal tax ID</Text>
+                        <Text>Legal Name: Hearts United for Animals</Text>
+                        <Text>Address: PO Box 286</Text>
+                        <Text>Auburn, NE 68305</Text>
+
+                        <Text>Federal Tax ID: 47-0773858</Text>
+
+                        <Text>Please call us at 1-402-274-3679 or email at hua@hua.org for additional information</Text>
                     </View>
-                    <Text style={styles.loadingText}>By Email</Text><Text></Text>
-                    <Text>You can email us at hua@hua.org.  Email inquiries receive the quickest response. </Text><Text></Text> 
-
-                    <Text>Please don't send email with attachments unless it's very clear in the subject line what the attachment is (like an application). We get thousands of emails a day and delete the ones that may be a virus. For the same reason, be sure to include a subject line that doesn't say "Hi" (or something similar) that may appear to be a virus.</Text>
-                    <Text style={styles.loadingText}>By Phone or Fax</Text>
-                    <Text>You can call us at 402.274.3679.</Text>
-
-                    <Text>You can send a fax to 402.274.3689.</Text>
-
-                    <Text>We'll be waiting!</Text>
-                    <Text>Visit HUA</Text>
-                    <Text>Visitation Policy</Text>
-                    <Text>Once your application is fully approved, we will reach out to schedule a time to visit the shelter and meet your new best friend. Adoption appointment hours are 10 am to 2 pm every day except Wednesdays. We are volunteer administration and need to make sure someone will be available to help you when you arrive. For volunteering and tours, please write to hua@hua.org to schedule a time to visit and obtain directions. We are in the country between Auburn and Nebraska City. We cannot be located via any mapping programs.</Text>
-                    <Text>INCLUDE HUA IN ESTATE PLANNING</Text>
-                    <Text>If you would like to include Hearts United for Animals in your will or trust, please use our legal name and federal tax ID</Text>
-                    <Text>Legal Name: Hearts United for Animals</Text>
-                    <Text>Address: PO Box 286</Text>
-                    <Text>Auburn, NE 68305</Text>
-
-                    <Text>Federal Tax ID: 47-0773858</Text>
-
-                    <Text>Please call us at 1-402-274-3679 or email at hua@hua.org for additional information</Text>
-                </View>
+                </ScrollView>
             </Animatable.View>
         );
     }
--- components/SearchAnimalsComponent.js
+++ components/SearchAnimalsComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Text, View, ScrollView, StyleSheet,
-    Picker, Button, Modal } from 'react-native';
+    Picker, Button, Modal, Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 class SearchAnimals extends Component {
 
@@ -27,6 +28,54 @@ class SearchAnimals extends Component {
     handleSearch() {
         console.log(JSON.stringify(this.state));
         this.toggleModal();
+
+        // Alert.alert(
+        //     'Begin Search?',
+        //     'Number of Campers: ' + this.state.campers + "\n" +
+        //     'Hike in? ' + this.state.hikeIn + "\n" +
+        //     'Date: ' + this.state.date,
+        //     [
+        //         {
+        //             text: 'Cancel',
+        //             style: 'cancel',
+        //             onPress: () => this.resetForm()
+        //         },
+        //         {
+        //             text: 'OK',
+        //             onPress: () => {
+        //                 this.presentLocalNotification(this.state.animalSize);
+        //                 this.resetForm();
+        //             }
+        //         }
+        //     ],
+        //     { cancelable: false }
+        // );
+    }
+
+    async presentLocalNotification(animalSize, animalAge, animalBreed, animalGender) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Animal Search',
+                    body: `Search for size: ${animalSize}, age: ${animalAge}, breed: ${animalBreed} and gender: ${animalGender} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     resetForm() {
@@ -89,7 +138,12 @@ class SearchAnimals extends Component {
                     <Text style={styles.formLabel}>Animal Gender</Text>
                     <Picker
                         style={styles.formItem}
-                        selectedValue={this.state.animalGender}
+                        selectedValue={
+                            this.state.animalGender,
+                            this.state.animalAge,
+                            this.state.animalBreed,
+                            this.state.animalGender
+                        }
                         onValueChange={itemValue => this.setState({animalGender: itemValue})}
                     >
                         <Picker.Item label='Select' value='Not Selected' />
@@ -126,6 +180,15 @@ class SearchAnimals extends Component {
                         <Text style={styles.modalText}>
                             Animal Gender: {this.state.animalGender}
                         </Text>
+                        <Button
+                            onPress={() => {
+                                this.toggleModal();
+                                this.presentLocalNotification(this.state.animalSize, this.state.animalAge, this.state.animalBreed, this.state.animalGender);
+                                this.resetForm();
+                            }}
+                            color='#5637DD'
+                            title='Search'
+                        />
                         <Button
                             onPress={() => {
                                 this.toggleModal();
